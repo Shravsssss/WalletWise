280fd24b6073c5f4381d3d247eaa55904a0978e1
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-12-03 22:59:05 -0500

    Fix coding and styling issues for history.py

--- src/history.py
+++ src/history.py
@@ -1,10 +1,11 @@
-import helper
 import logging
-from pymongo_run import get_database
- 
- 
- 
+# Module providing helper functions
+from . import helper
+from .pymongo_run import get_database
+
+
 def run(message, bot):
+    """This is the run function"""
     try:
         chat_id = message.chat.id
         user_data = None
@@ -16,24 +17,27 @@ def run(message, bot):
                 user_data = i
         if user_data is None:
             raise Exception("Sorry! No spending records found!")
- 
-        spend_total_str = "Here is your spending history : \nDATE, CATEGORY, AMOUNT\n----------------------\n"
+
+        spend_total_str = """Here is your spending history : \nDATE
+        , CATEGORY, AMOUNT\n----------------------\n"""
         if len(user_data) == 0:
             spend_total_str = "Sorry! No spending records found!"
         else:
             for rec in user_data["personal_expenses"]:
                 spend_total_str += str(rec) + "\n"
- 
+
             transactions_list = helper.get_group_expenses_file()
             for transaction_id in user_data["group_expenses"]:
                 if transaction_id not in transactions_list:
-                    raise Exception("An unknown transaction was found in your records, please try again later.")
+                    exception_message = """An unknown transaction was found
+                    in your records, please try again later."""
+                    raise Exception(exception_message)
                 txn = transactions_list[transaction_id]
-                rec = txn["created_at"] + "," + txn["category"] + "," + str(txn["members"][str(chat_id)])
+                rec = txn["created_at"] + "," + txn["category"]
+                rec += "," + str(txn["members"][str(chat_id)])
                 spend_total_str += str(rec) + "\n"
- 
+
         bot.send_message(chat_id, spend_total_str)
-    except Exception as e:
-        logging.exception(str(e))
-        bot.reply_to(message, "Oops!" + str(e))
- 
+    except Exception as exception_value:
+        logging.exception(str(exception_value))
+        bot.reply_to(message, "Oops!" + str(exception_value))
