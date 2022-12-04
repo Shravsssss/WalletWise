8e47a28b3a8fcd03d64a40222a416f7d898f9a1e
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-12-04 04:59:00 -0500

    Fix coding and styling issues for test_start_menu_command.py

--- test/test_start_menu_command.py
+++ test/test_start_menu_command.py
@@ -7,6 +7,7 @@
 
 
 def test_start_and_menu_command_func():
+    """This is the test function for start and menu command method"""
     # test_result = start_and_menu_command("/start")
     # assert True == test_result, "Normal Case"
     print("Hello")
