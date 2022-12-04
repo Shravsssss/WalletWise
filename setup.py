847902958461dc038df0a7c801c30148a7903791
tusharkini <kini1998pv@gmail.com>
2022-12-03 22:35:33 -0500

    Update setup.py file

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='WalletBuddy',
     version='1.0',
-    description='An easy to use Telegram Bot to track everyday expenses with your friends',
+    description='An easy to use Telegram Bot to track | '
+                'everyday expenses with your friends',
     author='Boscosylvester, Tushar, Shlok, Prasad, Ankur',
-    scripts=['main/main.py'],
+    scripts=['src/main.py'],
     packages=find_packages()
 )   
