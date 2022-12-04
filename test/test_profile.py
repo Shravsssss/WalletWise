1fd09fd97953ecf8f384f52d76d30a2f6df0df46
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-12-04 04:57:28 -0500

    Fix coding and styling issues for test_profile.py

--- test/test_profile.py
+++ test/test_profile.py
@@ -2,38 +2,39 @@ from mock import patch
 from telebot import types
 from src import profile
 
-dateFormat = '%d-%b-%Y'
-timeFormat = '%H:%M'
-monthFormat = '%b-%Y'
+DATE_FORMAT = '%d-%b-%Y'
+TIME_FORMAT = '%H:%M'
+MONTH_FORMAT = '%b-%Y'
+
 
 def create_message(text):
+    """This is the funtion to create message"""
     params = {'messagebody': text}
     chat = types.User(11, False, 'test')
     return types.Message(1, None, None, chat, 'text', params, "")
 
+
 @patch('telebot.telebot')
-def test_run(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.reply_to.return_value = True
+def test_run(mock_telebot):
+    """This is the test funtion to run"""
+    mock_value = mock_telebot.return_value
+    mock_value.reply_to.return_value = True
     message = create_message("hello from test run!")
-    profile.run(message, mc)
+    profile.run(message, mock_value)
 
 
 @patch('telebot.telebot')
-def test_post_email_input(mock_telebot, mocker):
-
-    mc = mock_telebot.return_value
-    mc.reply_to.return_value = True
+def test_post_email_input(mock_telebot):
+    """This is the test function to post email input"""
+    mock_value = mock_telebot.return_value
+    mock_value.reply_to.return_value = True
     profile.add_group.validate_email_input.return_value = True
     # profile.helper.read_json.return_value = {}
     message = create_message("pbrr@gmail.com")
     message.text = "pbrr@gmail.com"
     # profile.helper.write_json.return_value = True
-    profile.post_email_input(message, mc)
+    profile.post_email_input(message, mock_value)
 
     # call exceptions
     message.text = None
-    profile.post_email_input(message, mc)
-
-
-
+    profile.post_email_input(message, mock_value)
