c5ef8a836115cdb5f3e01b229dc8fe6449b89f98
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-12-04 04:53:27 -0500

    Fix coding and styling issues for test_history.py

--- test/test_history.py
+++ test/test_history.py
@@ -4,6 +4,7 @@ from src import history
 
 
 def create_message(text):
+    """This is the create message function"""
     params = {'text': text}
     chat = types.User(11, False, 'test')
     return types.Message(1, None, None, chat, 'text', params, "")
@@ -11,51 +12,66 @@ def create_message(text):
 
 @patch('telebot.telebot')
 def test_run(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.reply_to.return_value = True
-    mc = mock_telebot.return_value
+    """This is the test funtion for run method"""
+    mock = mock_telebot.return_value
+    mock.reply_to.return_value = True
+    mock = mock_telebot.return_value
     mocker.patch.object(history, 'helper')
 
-    transaction_list = {"1001": {"created_at": "", "category": "", "members": {"11": 0}},
-                        "1002": {"created_at": "", "category": "", "members": {"11": 0, "20": 0}}
-                        }
+    # transaction_list = {
+    #     "1001": {
+    #         "created_at": "",
+    #         "category": "",
+    #         "members": {"11": 0}
+    #     },
+    #     "1002": {
+    #         "created_at": "",
+    #         "category": "",
+    #         "members": {"11": 0, "20": 0}
+    #     }
+    # }
     # history.helper.read_json.side_effect = [None, transaction_list]
-    history.helper.get_user_history.return_value = {'personal_expenses': "sample expense record", "group_expenses": ["1002"]}
+    history.helper.get_user_history.return_value = {
+        'personal_expenses': "sample expense record",
+        "group_expenses": ["1002"]
+    }
 
     message = create_message("Hello from testing")
     try:
-        history.run(message, mc)
-    except Exception as e:
+        history.run(message, mock)
+    except Exception:
         assert True
 
 
 @patch('telebot.telebot')
 def test_run_no_records(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.reply_to.return_value = True
-    mc = mock_telebot.return_value
+    """This is the test function for run no records method"""
+    mock = mock_telebot.return_value
+    mock.reply_to.return_value = True
+    mock = mock_telebot.return_value
     mocker.patch.object(history, 'helper')
     # history.helper.read_json.return_value = None
     history.helper.get_user_history.return_value = None
 
     message = create_message("Hello from testing")
     try:
-        history.run(message, mc)
-    except Exception as e:
+        history.run(message, mock)
+    except Exception:
         assert True
 
 
 @patch('telebot.telebot')
 def test_run_empty_records(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.reply_to.return_value = True
-    mc = mock_telebot.return_value
+    """This is the test function for run empty records method"""
+    mock = mock_telebot.return_value
+    mock.reply_to.return_value = True
+    mock = mock_telebot.return_value
     mocker.patch.object(history, 'helper')
     # history.helper.read_json.return_value = None
     history.helper.get_user_history.return_value = []
 
     message = create_message("Hello from testing")
     try:
-        history.run(message, mc)
-    except Exception as e:
+        history.run(message, mock)
+    except Exception:
         assert True
