a3304d9bb8ca99d8dae890eb0ea844453ecf3f2a
shlokio <112107528+shlokio@users.noreply.github.com>
2022-12-05 22:00:02 -0500

    Add settle up functionality

--- src/settleup.py
+++ src/settleup.py
@@ -1,7 +1,7 @@
 import logging
 from . import helper
 from .pymongo_run import get_database
-
+from . import add_group
 
 def run(message, bot):
     """This is the run function"""
@@ -18,8 +18,23 @@ def post_email_input(message, bot):
             raise Exception(f"Sorry the email format is not correct: {email}")
         print("----SUCCESS!----")
         dbname = get_database()
-        
-
+        collection_name = dbname["USER_EMAILS"]
+        item_details = collection_name.find()
+        user_data = None
+        for i in item_details:
+            #print(i)
+            if list(i.values())[1] == email:
+                user_data = email
+        if user_data is None:
+            raise Exception("Sorry! email isn't present in database!")
+        message = bot.send_message(
+            chat_id,
+            "Enter amount for settling up \n(Enter numeric values only)"
+        )
+        bot.register_next_step_handler(
+            message,
+            post_amount_input,
+            bot        )
     except Exception as exception:
         logging.exception(str(exception))
         bot.reply_to(message, 'Oh no! ' + str(exception))
@@ -31,3 +46,25 @@ def post_email_input(message, bot):
             display_text += command_value + "\n"
         bot.send_message(chat_id, 'Please select a menu option from below:')
         bot.send_message(chat_id, display_text)
+
+
+
+def post_amount_input(message, bot):
+    """This is the post amount input function"""
+    try:
+        chat_id = message.chat.id
+        amount_entered = message.text
+        # validate
+        amount_value = helper.validate_entered_amount(amount_entered)
+        if amount_value == 0:  # cannot be $0 spending
+            raise Exception("Spent amount has to be a non-zero number.")
+        print("----SUCCESS!----")
+        dbname = get_database()
+        collection_name = dbname["USER_EXPENSES"]
+
+        bot.send_message(chat_id,
+            "----SETTLED UP!---"
+        )
+    except Exception as exception_value:
+        logging.exception(str(exception_value))
+        bot.reply_to(message, 'Oh no. ' + str(exception_value))
