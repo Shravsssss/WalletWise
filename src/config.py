971debdaa239380848ba7a649ec532874d918a77
Firasat Hussain Mohammed <mfirasathussain@gmail.com>
2024-10-31 17:51:30 -0400

    added categories for crypto coins

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 TOKEN = "8113186837:AAEu20LqkGTx2CGS9lqunMuvDw1JzUAPJx8"
-ExpenseCategories = [
-    'Food', 'Groceries', 'Utilities',
-    'Transport', 'Shopping', 'Miscellaneous']
+ExpenseCategories = ['Food', 'Groceries', 'Utilities', 'Transport', 'Shopping', 'Miscellaneous']
+CryptoCategories = ['Bitcoin', 'Ethereum', 'Ripple', 'Litecoin']
 ExpenseChoices = ['Date', 'Category', 'Cost']
 DisplayChoices = ['All Expenses', 'Category Wise', 'Shared Expense']
