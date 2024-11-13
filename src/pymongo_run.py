0cfb80f15aa6df8e01203a26da3e7a2e4cf31dcf
Panky Thakur~ <pankajmthakur01@gmail.com>
2024-11-12 20:23:20 -0500

    Fixed pymongo

--- src/pymongo_run.py
+++ src/pymongo_run.py
@@ -1,3 +1,4 @@
+import pymongo
 from pymongo import MongoClient
 
 
@@ -5,7 +6,10 @@ def get_database():
     """This is the get database function"""
     connection_string = """mongodb+srv://Firasat:GooglePixel@walletbuddy.hkpji.mongodb.net/WalletBuddy"""
     client = MongoClient(connection_string)
+    client = pymongo.MongoClient("mongodb+srv://<username>:<password>@walletbuddy.hkpji.mongodb.net/<dbname>?retryWrites=true&w=majority", ssl=True)
+
     return client['telebot']
+
 
 
 if __name__ == "__main__":  
