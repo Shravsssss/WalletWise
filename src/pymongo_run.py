7e2ec9fa55376777f91e68e4569739029c117c95
Sravya Yepuri <sravya.bg@gmail.com>
2024-11-26 18:08:22 +0000

    Edit APIToken

--- src/pymongo_run.py
+++ src/pymongo_run.py
@@ -1,17 +1,12 @@
-import pymongo
 from pymongo import MongoClient
 
 
 def get_database():
     """This is the get database function"""
-    connection_string = """mongodb+srv://shlokio:sLA3uByHAl844tQ5@cluster0.y9kiowx.mongodb.net/test"""
+    connection_string = """mongodb+srv://syepuri:syepuri@se-project3.0htqg.mongodb.net/?retryWrites=true&w=majority&appName=SE-Project3"""
     client = MongoClient(connection_string)
-    client = pymongo.MongoClient(
-        "mongodb+srv://<username>:<password>@walletbuddy.hkpji.mongodb.net/<dbname>?retryWrites=true&w=majority",
-        ssl=True)
-
     return client['telebot']
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
     dbname = get_database()
